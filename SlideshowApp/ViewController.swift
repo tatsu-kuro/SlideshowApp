88173741dd4420b661554563378529064c32106a
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2018-01-22 22:28:29 +0900

    Viewをクリックして画面遷移させる方法が分からないので、文字無しのボタンをViewと同じ位置に配置。反則でしょうが、とりあえず。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    @IBAction func playPhoto(_ sender: Any) {
+    }
+    @IBAction func lastPhoto(_ sender: Any) {
+    }
+    @IBAction func nextPhoto(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+//        let resultViewController:ResultViewController = //////segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+    //    resultViewController.x = 1
+     //   resultViewController.y = 1
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 
 }
 
