fbd92b5882983259388f1eab8b95b2e91f9c4f03
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2018-01-30 20:47:41 +0900

    完成したかな

--- SlideshowApp/BigViewController.swift
+++ SlideshowApp/BigViewController.swift
@@ -10,119 +10,16 @@ import UIKit
 
 class BigViewController: UIViewController {
     var fileName:String = ""
-    var imageView:UIImageView!
-    var scale:CGFloat = 1.0
-    var width:CGFloat = 0
-    var height:CGFloat = 0
-    var screenWidth:CGFloat = 0
-    var screenHeight:CGFloat = 0
-    var startPoint:CGPoint!
-    var endPoint:CGPoint!
-    // タッチ開始時のUIViewのorigin
-    var orgOrigin: CGPoint!
-    // タッチ開始時の親ビュー上のタッチ位置
-    var orgParentPoint : CGPoint!
-  
  
-    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
-        self.dismiss(animated: true, completion:nil)
-    }
-    
-    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
+    @IBOutlet weak var bigView: UIImageView!
   
-        switch sender.state {
-        case UIGestureRecognizerState.began:
-            // タッチ開始:タッチされたビューのoriginと親ビュー上のタッチ位置を記録しておく
-            orgOrigin = sender.view?.frame.origin
-            orgParentPoint = sender.translation(in: self.view)
-            break
-        case UIGestureRecognizerState.changed:
-            // 現在の親ビュー上でのタッチ位置を求める
-            let newParentPoint = sender.translation(in: self.view)
-            // パンジャスチャの継続:タッチ開始時のビューのoriginにタッチ開始からの移動量を加算する
-            sender.view?.frame.origin.x = orgOrigin.x + newParentPoint.x - orgParentPoint.x
-            sender.view?.frame.origin.y = orgOrigin.y + newParentPoint.y - orgParentPoint.y
-            break
-        default:
-            break
-        }
-  }
- 
-    func dispBigview(_ fn:String){
-        // Screen Size の取得
-        screenWidth = self.view.bounds.width
-        screenHeight = self.view.bounds.height
-        
-        // UIImage インスタンスの生成
-        let image = UIImage(named:fn)!
-        
-        // 画像の幅・高さの取得
-        width = image.size.width
-        height = image.size.height
-        
-        // UIImageView インスタンス生成
-        imageView = UIImageView(image:image)
-        
-        // 画像サイズをスクリーン幅に合わせる
-        scale = screenWidth / width
-        //        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        
-        // ImageView frame をCGRectで作った矩形に合わせる
-        imageView.frame = rect;
-        
-        // 画像の中心をスクリーンの中心位置に設定
-        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
-        
-        // view に ImageView を追加する
-        self.view.addSubview(imageView)
-        
-        //       initImageView()
-        // Do any additional setup after loading the view, typically from a nib.
-
-    }
-    func initImageView(_ fn:String){
-        // UIImage インスタンスの生成
-        // 画像はAssetsに入れてないのとjpgなので拡張子を入れます
-        let image1:UIImage = UIImage(named:fn)!
-        
-        // UIImageView 初期化
-        let imageView = UIImageView(image:image1)
-        // 画像の幅・高さの取得
-        let width = image1.size.width
-        let height = image1.size.height
-        
-        // 画面の横幅を取得
-   //     let screenWidth:CGFloat = view.frame.size.width
-   //     let screenHeight:CGFloat = view.frame.size.height
-        let screenWidth = self.view.bounds.width
-        let screenHeight = self.view.bounds.height
-        // 画像サイズをスクリーン幅の３倍に合わせる
-        scale = 2*screenWidth / width
-        //        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        // ImageView frame をCGRectで作った矩形に合わせる
-        imageView.frame = rect;
-        // 画像の中心を画面の中心に設定
-        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
-        imageView.center = CGPoint(x:205,y:255)
-        // UIImageViewのインスタンスをビューに追加
-        self.view.addSubview(imageView)
-        
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-       // print(fileName1)
-       //dispBigview(fileName)
-        initImageView(fileName)
-        // Do any additional setup after loading the view.
+        bigView.image = #imageLiteral(resourceName: fileName)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
-
 }
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -23,51 +23,15 @@ class ViewController: UIViewController  {
        //プロジェクトに追加されたファイルを自動的に登録したいが、解らない
     fileNames=["IMG_1448.jpg","IMG_1454.jpg","IMG_1469.jpg","IMG_2461.jpg"]
         fileCnt = fileNames.count
- 
     }
     
     // selector: #selector(updatetimer) で指定された関数
-    // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
     @objc func updateTimer(timer: Timer) {
         if playF {
-//          nextPhoto(Int())//ok
-//          nextPhoto(fileNumber)//ok
-//          nextPhoto(fileNumber)//ok
-            nextPhoto(!)
+            nextPhoto(0)
       }
     }
-    func dispSmallview(_ fn:String){
-        // Screen Size の取得
-        screenWidth = self.view.bounds.width
-        screenHeight = self.view.bounds.height
- //       smallView.image = fileNames[0]
-         smallView.image = #imageLiteral(resourceName: fn)
-  /*      // UIImage インスタンスの生成
- //       let image = UIImage(named:fn)!
-        
-        // 画像の幅・高さの取得
-        width = (smallView.image?.size.width)!
-        height = (smallView.image?.size.height)!
-        
-        // UIImageView インスタンス生成
-  //      imageView = UIImageView(image:image)
-        
-        // 画像サイズをスクリーン幅に合わせる
-        scale = screenWidth / width
-        //        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
-        
-        // ImageView frame をCGRectで作った矩形に合わせる
-        imageView.frame = rect;
-        
-        // 画像の中心をスクリーンの中心位置に設定
-        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
-        
-        // view に ImageView を追加する
-        self.view.addSubview(imageView)
-    */
-    }
-   
+ 
     @IBOutlet weak var nextB: UIButton!
     @IBOutlet weak var lastB: UIButton!
     @IBOutlet weak var playButton: UIButton!
@@ -86,35 +50,29 @@ class ViewController: UIViewController  {
             
         }
     }
+    
     @IBAction func lastPhoto(_ sender: Any) {
         fileNumber -= 1
         if fileNumber < 0 {
             fileNumber = fileCnt-1
         }
-        dispSmallview(fileNames[fileNumber])
-    }
+        smallView.image = #imageLiteral(resourceName: fileNames[fileNumber])
+     }
+    
     @IBAction func nextPhoto(_ sender: Any) {
         fileNumber += 1
         if fileNumber > fileCnt-1{
             fileNumber = 0
         }
-        dispSmallview(fileNames[fileNumber])
+        smallView.image = #imageLiteral(resourceName: fileNames[fileNumber])
     }
-    var imageView:UIImageView!
-    var scale:CGFloat = 1.0
-    var width:CGFloat = 0
-    var height:CGFloat = 0
-    var screenWidth:CGFloat = 0
-    var screenHeight:CGFloat = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        
- //       smallView.image = #imageLiteral(resourceName: "IMG_1448.jpg")
-        
         setFilenames()
-        dispSmallview(fileNames[fileNumber])
+        smallView.image = #imageLiteral(resourceName: fileNames[fileNumber])
     }
 
     override func didReceiveMemoryWarning() {
@@ -126,6 +84,9 @@ class ViewController: UIViewController  {
  
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
+        if playF {
+            playPhoto(0)
+        }
         let bBigViewController:BigViewController = segue.destination as! BigViewController
         bBigViewController.fileName = fileNames[fileNumber]
     }
@@ -133,8 +94,6 @@ class ViewController: UIViewController  {
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
         // 他の画面から segue を使って戻ってきた時に呼ばれる
     }
-    /*
-    @IBAction func unwindToTop(segue: UIStoryboardSegue) {
-    }*/
+
 }
 
